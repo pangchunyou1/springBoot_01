720a11cde0354da1d60e285942c7636158b41624
庞春有 <13078298430@163.com>
2024-04-27 22:46:15 +0800

    初始化本地仓库。第er次提交

--- src/main/java/com/atguigu/springBoot/bean/Student.java
+++ src/main/java/com/atguigu/springBoot/bean/Student.java
@@ -2,6 +2,6 @@ package com.atguigu.springBoot.bean;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("args = " + args);
+        System.out.println("args 222= " + args);
     }
 }
