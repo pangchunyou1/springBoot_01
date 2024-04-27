94ee528e096812bd9b2afc48464f2256100cb722
庞春有 <13078298430@163.com>
2024-04-28 00:15:02 +0800

    第san次提交

--- src/main/java/com/atguigu/springBoot/bean/Student.java
+++ src/main/java/com/atguigu/springBoot/bean/Student.java
@@ -3,5 +3,7 @@ package com.atguigu.springBoot.bean;
 public class Student {
     public static void main(String[] args) {
         System.out.println("args 222= " + args);
+        System.out.println("\"1111111\" = " + "1111111");
+
     }
 }
